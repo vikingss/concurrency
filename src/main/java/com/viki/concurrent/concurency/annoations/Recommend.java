01de0bd3910a845f6e0e7d8a07b54404b5d3f080
xunruibo <xunruibo@autohome.com.cn>
2018-04-23 16:00:14 +0800

    init

---
+++ src/main/java/com/viki/concurrent/concurency/annoations/Recommend.java
@@ -0,0 +1,18 @@
+package com.viki.concurrent.concurency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author: xunruibo
+ * @Description:
+ * @Date: Created in 15:18 2018/4/23
+ * @Modified By:
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+    String value() default "";
+}
