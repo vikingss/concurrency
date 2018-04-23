f11bed9df22bf078fc3be168361b02cc70b94f84
xunruibo <xunruibo@autohome.com.cn>
2018-04-23 15:15:29 +0800

    init

---
+++ src/main/java/com/viki/concurrent/concurency/ConcurencyApplication.java
@@ -0,0 +1,12 @@
+package com.viki.concurrent.concurency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurencyApplication.class, args);
+	}
+}
